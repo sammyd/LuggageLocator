8ab71492125b541d460954e6d8849b76f9a7a19a
Sam Davies <sam@visualputty.co.uk>
2014-08-30 16:10:34 +0100

    Initial commit of Luggage Locator app

---
+++ Luggage Locator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Luggage Locator
+//
+//  Created by Sam Davies on 30/08/2014.
+//  Copyright (c) 2014 VisualPutty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
